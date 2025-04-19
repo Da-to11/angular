edfaa301ccf6c9a91869f8cd4319cc7e142c8e5a
Default <papavadato89@gmail.com>
2025-04-19 05:48:19 -0700

    HW 3 angular

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+
+  textArr : string[] = []
+
+  text = ""
+
+
+  add(){
+    this.textArr.push(this.text)
+  }
+
+
+  show(){
+    for(let el of this.textArr){
+      console.log(el)
+    }
+  }
+
+}
