3218dfaab0900a191dd58a313c62648faca9282b
Default <papavadato89@gmail.com>
2025-05-02 07:45:04 -0700

    ang 6 hw

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+import { ProdactComponent } from './prodact/prodact.component';
+import { ProdactDetalsComponent } from './prodact/prodact-detals/prodact-detals.component';
+
+
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'users', component: UsersComponent}
+    {path: 'users', component: UsersComponent},
+    {path: 'prodact', component: ProdactComponent},
+    {path: 'prodact/:id', component: ProdactDetalsComponent}
 ];
